af6ea1434fe4749d346dfaedb9c6f2be889d5f97
Vasanth5549 <88388661+Vasanth5549@users.noreply.github.com>
2025-04-22 15:23:33 +0530

    Delete DIT_Package_84782/DIT_Package_84782 directory

--- DIT_Package_84782/DIT_Package_84782/undeploy.bat
+++
@@ -1,24 +0,0 @@
-:: Replace set variable to your environment.
-@echo off
-SETLOCAL
-echo Enter the username to connect runtime:
-set /p UNAME=""
-echo Enter the password to connect runtime:
-set /p PASSWORD=""
-IF "%UNAME%"=="" set result=true
-IF "%PASSWORD%"=="" set result=true
-IF "%result%"=="true" (
-echo The mandatory Username or Password parameters for Runtime is not available. Cannot proceed with process deployment
- )
-set ADDRESS="localhost"
-set PORT="1617"
-
-java -jar vclu.jar  --undeploy --jmx --username %UNAME% --password %PASSWORD% --address %ADDRESS% --port %PORT% --processname CustomMessageRouter
-
-java -jar vclu.jar  --undeploy --jmx --username %UNAME% --password %PASSWORD% --address %ADDRESS% --port %PORT% --processname ORBISTransformOut
-
-java -jar vclu.jar  --undeploy --jmx --username %UNAME% --password %PASSWORD% --address %ADDRESS% --port %PORT% --processname ORBISLorenzoConnectionIn
-
-java -jar vclu.jar  --undeploy --jmx --username %UNAME% --password %PASSWORD% --address %ADDRESS% --port %PORT% --processname ORBISTransformIn
-ENDLOCAL
-
